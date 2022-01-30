3ca0292f4997d5c52adae0653aaf586472c0e2d7
samugs13 <garciasanchezsamu@gmail.com>
2022-01-30 16:24:10 +0100

    added multimonitor widgets

--- qtile/settings/decorwidgets.py
+++ qtile/settings/decorwidgets.py
@@ -206,3 +206,339 @@ altwidgets = [
         length=8,
     ),
 ]
+
+altwidgets2 = [
+
+    widget.GroupBox(
+       font = 'Font Awesome 5 Free Solid',
+       active = onedark[4],
+       inactive = onedark[7],
+       highlight_method="border",
+       borderwidth = 2,
+       this_current_screen_border = onedark[4],
+       block_highlight_text_color = onedark[7],
+       highlight_color = onedark[0],
+       center_aligned = True,
+       background = onedark[0],
+       fontsize = 14,
+       disable_drag = True,
+       padding_y = 7,
+       padding_x = 3,
+       visible_groups=[fa.icons['terminal'], fa.icons['globe'], fa.icons['folder-open'], fa.icons['code'], fa.icons['desktop']],
+    ),
+
+    widget.Sep(),
+    
+    widget.WindowName(
+        padding = 7,
+        max_chars = 200,
+        foreground = onedark[7],
+        font = 'Hack Bold',
+        fontsize = 14,
+        empty_group_string = 'Desktop'
+    ),
+
+    widget.Spacer(length=bar.STRETCH),
+    
+    widget.Clock(
+    font = 'Font Awesome 5 Free',
+    foreground=onedark[0],
+    format= fa.icons['clock'] + ' %H:%M ' + fa.icons['calendar-alt'] + ' %a, %d de %h de %Y',
+    **rect),
+     
+    widget.Spacer(length=bar.STRETCH),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['sun'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **redb
+    ),
+
+    widget.Backlight(
+        backlight_name = 'intel_backlight',
+        brightness_file = 'brightness',
+        fmt = '{}',
+        padding = 5,
+        foreground = onedark[7],
+        background = onedark[0],
+    ),
+    
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['keyboard'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **purpleb
+    ),
+
+    widget.KeyboardLayout(
+        configured_keyboards = ['es','us'],
+        fmt = '{}',
+        background = onedark[0],
+        padding = 5,
+        foreground = onedark[7], 
+    ), 
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['user-secret'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **greenb
+    ),
+
+    widget.TextBox(
+        text = ip,
+        padding = 5,
+        foreground = onedark[7],
+        background = onedark[0],
+    ),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['sync'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **cyanb
+    ),
+
+    widget.CheckUpdates(
+        update_interval=20,
+        foreground = onedark[7],
+        colour_have_updates = onedark[7],
+        colour_no_updates = onedark[7],
+        distro = 'Arch_checkupdates',
+        display_format = '{updates} updates',
+        no_update_string = 'updated',
+        padding = 5,
+        mouse_callbacks = {"Button1": lambda: qtile.cmd_spawn(terminal + " -e sudo pacman -Syu")},
+        background = onedark[0],
+    ),
+
+     widget.Spacer(
+        length = 5,
+    ),
+
+    widget.CurrentLayoutIcon(
+        foreground = onedark[7],
+        padding = 0,
+        scale = 0.45,
+        background = onedark[0],
+        **yellowb
+    ),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.CurrentLayout(
+        foreground = onedark[7],
+        padding = 0,
+        background = onedark[0],
+        fmt = '{} '
+    ),
+    
+    widget.Sep(),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.LaunchBar(
+        progs=[
+            (fa.icons['discord'], 'brave --new-window https://www.discord.com', 'Discord'),
+            (fa.icons['github'], 'brave --new-window https://www.github.com', 'Github'),
+            (fa.icons['reddit-alien'], 'brave --new-window https://www.reddit.com', 'Reddit'),
+            (fa.icons['twitch'], 'brave --new-window https://www.twitch.com', 'Twitch'),
+            (fa.icons['youtube'], 'brave --new-window https://www.youtube.com', 'Youtube'),
+            (fa.icons['spotify'], 'spotify', 'Spotify'),
+            (fa.icons['skype'], 'skypeforlinux', 'Skype'),
+        ],
+    ),
+
+    widget.Spacer(
+        length=8,
+    ),
+]
+
+altwidgets3 = [
+
+    widget.GroupBox(
+       font = 'Font Awesome 5 Free Solid',
+       active = onedark[4],
+       inactive = onedark[7],
+       highlight_method="border",
+       borderwidth = 2,
+       this_current_screen_border = onedark[4],
+       block_highlight_text_color = onedark[7],
+       highlight_color = onedark[0],
+       center_aligned = True,
+       background = onedark[0],
+       fontsize = 14,
+       disable_drag = True,
+       padding_y = 7,
+       padding_x = 3,
+       visible_groups=[fa.icons['video'], fa.icons['edit'], fa.icons['music'], fa.icons['comment-dots'], fa.icons['skull-crossbones']],
+    ),
+
+    widget.Sep(),
+    
+    widget.WindowName(
+        padding = 7,
+        max_chars = 200,
+        foreground = onedark[7],
+        font = 'Hack Bold',
+        fontsize = 14,
+        empty_group_string = 'Desktop'
+    ),
+
+    widget.Spacer(length=bar.STRETCH),
+    
+    widget.Clock(
+    font = 'Font Awesome 5 Free',
+    foreground=onedark[0],
+    format= fa.icons['clock'] + ' %H:%M ' + fa.icons['calendar-alt'] + ' %a, %d de %h de %Y',
+    **rect),
+     
+    widget.Spacer(length=bar.STRETCH),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['sun'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **redb
+    ),
+
+    widget.Backlight(
+        backlight_name = 'intel_backlight',
+        brightness_file = 'brightness',
+        fmt = '{}',
+        padding = 5,
+        foreground = onedark[7],
+        background = onedark[0],
+    ),
+    
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['keyboard'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **purpleb
+    ),
+
+    widget.KeyboardLayout(
+        configured_keyboards = ['es','us'],
+        fmt = '{}',
+        background = onedark[0],
+        padding = 5,
+        foreground = onedark[7], 
+    ), 
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['user-secret'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **greenb
+    ),
+
+    widget.TextBox(
+        text = ip,
+        padding = 5,
+        foreground = onedark[7],
+        background = onedark[0],
+    ),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.TextBox(
+        font = 'Font Awesome 5 Free',
+        text = fa.icons['sync'],
+        padding = 8,
+        foreground = onedark[7],
+        background = onedark[0],
+        **cyanb
+    ),
+
+    widget.CheckUpdates(
+        update_interval=20,
+        foreground = onedark[7],
+        colour_have_updates = onedark[7],
+        colour_no_updates = onedark[7],
+        distro = 'Arch_checkupdates',
+        display_format = '{updates} updates',
+        no_update_string = 'updated',
+        padding = 5,
+        mouse_callbacks = {"Button1": lambda: qtile.cmd_spawn(terminal + " -e sudo pacman -Syu")},
+        background = onedark[0],
+    ),
+
+     widget.Spacer(
+        length = 5,
+    ),
+
+    widget.CurrentLayoutIcon(
+        foreground = onedark[7],
+        padding = 0,
+        scale = 0.45,
+        background = onedark[0],
+        **yellowb
+    ),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.CurrentLayout(
+        foreground = onedark[7],
+        padding = 0,
+        background = onedark[0],
+        fmt = '{} '
+    ),
+    
+    widget.Sep(),
+
+    widget.Spacer(
+        length = 5,
+    ),
+
+    widget.Systray(
+        padding = 2,
+    ),
+
+    widget.Spacer(
+        length=8,
+    ),
+]
