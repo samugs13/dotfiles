9efe552dd99f780b995785838cba1498c4d3fc27
samugs13 <garciasanchezsamu@gmail.com>
2023-10-08 20:37:04 +0200

    modified script

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -12,17 +12,17 @@ setxkbmap es  # Keyboard layout
 #feh --bg-fill /home/s4mb4/.config/qtile/settings/wallpapers/w003.png  # Set wallpaper 
 
 #starting utility applications at boot time
-run nm-applet &
+run nm-applet
 #run pamac-tray &
-run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
+run xfce4-power-manager
+numlockx on
+blueberry-tray
 #run cbatticon -i notification &   # Battery systray icon
-run volumeicon &
-run variety &
+run volumeicon
+run variety
 #wal -R  # -R restores the last colorscheme and wallpaper that was in use
-picom --config $HOME/.config/qtile/settings/scripts/picom.conf &
-dunst &
+picom --config $HOME/.config/qtile/settings/scripts/picom.conf
+dunst
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 run xautolock -time 10 -locker slock
 
