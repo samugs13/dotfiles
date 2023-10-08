9c1886a2b95102da5d54ddfe618c6d1c167930ff
samugs13 <garciasanchezsamu@gmail.com>
2023-10-08 20:51:02 +0200

    modified apps launched when starting awesome

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -11,12 +11,10 @@ setxkbmap es  # Keyboard layout
 
 #starting utility applications at boot time
 run nm-applet
-run xfce4-power-manager
 numlockx on
 blueberry-tray
 run cbatticon -i notification   # Battery systray icon
 run volumeicon
-run variety
 picom --config $HOME/.config/qtile/settings/scripts/picom.conf
 dunst
 run xautolock -time 10 -locker slock
