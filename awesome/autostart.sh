b8ecea8f66d9929b9f210d411658d9a442d05982
samugs13 <garciasanchezsamu@gmail.com>
2023-10-09 16:34:13 +0200

    start flameshot

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -15,5 +15,6 @@ numlockx on
 blueberry-tray
 run cbatticon -i notification   # Battery systray icon
 run volumeicon
+run flameshot
 picom --config $HOME/.config/qtile/settings/scripts/picom.conf
 run xautolock -time 10 -locker slock
